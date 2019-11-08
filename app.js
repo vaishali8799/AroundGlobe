44794ea975e1f71640c0f0af8cb200b9c5610494
vaishali8799 <vaishalikasondhan.8799@gmail.com>
2019-11-08 21:37:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+navBtn.addEventListener("click",() => {
+ links.classList.toggle("show-links");});
